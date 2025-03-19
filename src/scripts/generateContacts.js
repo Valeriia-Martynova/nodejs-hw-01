16381ccd057b9f855822471403c56104dafd702c
Valeriia Martynova <vmartynova95@gmail.com>
2025-03-19 22:27:09 +0200

    add code

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,11 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  const contactsList = await readContacts();
+  const newContacts = Array(number).fill(null).map(createFakeContact);
+  await writeContacts([...contactsList, ...newContacts]);
+};
 
 generateContacts(5);
